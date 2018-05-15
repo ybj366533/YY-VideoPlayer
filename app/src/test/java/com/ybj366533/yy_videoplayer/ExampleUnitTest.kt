feaed010e37d2226601a681157107721b7da26f1
Summer <ybj366533@qq.com>
2018-05-15 10:33:04 +0800

    1.初始化提交

---
+++ app/src/test/java/com/ybj366533/yy_videoplayer/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.ybj366533.yy_videoplayer
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
